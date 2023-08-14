29b80b9c5b88a60fb6467c39a4f64d1d90149479
tuanta <tanhtuan509@gmail.com>
2023-08-14 23:32:39 +0700

    push demo Dom  PARSE

---
+++ HTMLAgilityPack/HTMLAgilityPack/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using HTMLAgilityPack.Models;
+using Microsoft.AspNetCore.Mvc;
+using ReadHTMLClassLibrary;
+using System.Diagnostics;
+
+namespace HTMLAgilityPack.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.result = ReadHTML.SendList("https://lipsum.com/","//h2");
+            return View();
+        }
+    }
+}
---
+++ HTMLAgilityPack/ReadHTMLClassLibrary/ReadHTML.cs
@@ -0,0 +1,83 @@
+﻿using System.Net;
+
+namespace ReadHTMLClassLibrary
+{
+    public static class ReadHTML
+    {
+        public static string? Send(string url, string node)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(url) && !String.IsNullOrEmpty(node))
+                {
+                    WebClient webClient = new WebClient();
+                    webClient.Encoding = System.Text.Encoding.UTF8;
+                    string html = webClient.DownloadString(url);
+                    if (!String.IsNullOrEmpty(html))
+                    {
+                        HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+                        htmlDocument.LoadHtml(html);
+                        if (htmlDocument != null)
+                        {
+                            HtmlAgilityPack.HtmlNode htmlNode = htmlDocument.DocumentNode.SelectSingleNode(node);
+                            if (htmlNode != null)
+                            {
+                                string innerText = htmlNode.InnerText;
+                                if (innerText != null)
+                                {
+                                    return innerText;
+                                }
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+            catch
+            {
+
+                return null;
+            }
+        }
+
+        public static List<string> SendList(string url, string node)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(url) && !String.IsNullOrEmpty(node))
+                {
+                    WebClient webClient = new WebClient();
+                    webClient.Encoding = System.Text.Encoding.UTF8;
+                    string html = webClient.DownloadString(url);
+                    if (!String.IsNullOrEmpty(html))
+                    {
+                        HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+                        htmlDocument.LoadHtml(html);
+                        if (htmlDocument != null)
+                        {
+                            HtmlAgilityPack.HtmlNodeCollection htmlNodes = htmlDocument.DocumentNode.SelectNodes(node);
+                            if (htmlNodes != null && htmlNodes.Count > 0)
+                            {
+                                List<string> listNode = new List<string>();
+                                foreach (var item in htmlNodes)
+                                {
+                                    string innerText = item.InnerText;
+                                    if (!String.IsNullOrEmpty(innerText))
+                                    {
+                                        listNode.Add(innerText);
+                                    }
+                                }
+                                return listNode;
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
